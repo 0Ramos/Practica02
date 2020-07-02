4f16423f520bdbe1764ba64ca7d0ebd763024313
practica <stefany.040997@gmail.com>
2020-07-02 11:13:04 -0500

    Practica02

---
+++ Pregunta3.php
@@ -0,0 +1,17 @@
+
+<table border ="0" align="center">
+
+<?php
+for ($x=12; $x<=6; $x++)
+{
+    for ($y=12; $y<=6; $y++){
+        $multiplicacion =$x*$y;
+        echo"<td>";
+        echo $multiplicacion;
+        echo "</td>";
+
+}
+    echo "</tr>";
+}
+?>
+</table>
